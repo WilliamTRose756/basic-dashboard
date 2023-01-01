afb82cca031a6d203f03b174db1baacf0b7260f8
William_Rose <william.rose@modjoul.com>
2023-01-01 14:09:02 -0500

    Improve efficiency

--- src/components/DisplayTwo.jsx
+++ src/components/DisplayTwo.jsx
@@ -22,6 +22,25 @@ function DisplayTwo() {
     });
   }, []);
 
+  const zone1Data = [];
+  const zone2Data = [];
+  const zone3Data = [];
+  const zone4Data = [];
+
+  if (data) {
+    data.forEach((datum) => {
+      if (datum.location === "zone_1") {
+        zone1Data.push(datum);
+      } else if (datum.location === "zone_2") {
+        zone2Data.push(datum);
+      } else if (datum.location === "zone_3") {
+        zone3Data.push(datum);
+      } else if (datum.location === "zone_4") {
+        zone4Data.push(datum);
+      }
+    });
+  }
+
   return isLoading ? (
     <h2 style={{ textAlign: "center" }}>Loading...</h2>
   ) : (
@@ -36,53 +55,32 @@ function DisplayTwo() {
         <div className="container">
           <div className="inner-square">
             <ul style={{ paddingLeft: "0" }}>
-              {data &&
-                data.map((datum) => {
-                  return (
-                    datum.location === "zone_1" && (
-                      <DisplayItem datum={datum} key={datum.device} />
-                    )
-                  );
-                })}
+              {zone1Data.map((datum) => {
+                return <DisplayItem datum={datum} key={datum.device} />;
+              })}
+            </ul>
+          </div>
+          <div className="inner-square">
+            <ul style={{ paddingLeft: "0" }}>
+              {zone2Data.map((datum) => {
+                return <DisplayItem datum={datum} key={datum.device} />;
+              })}
             </ul>
           </div>
 
           <div className="inner-square">
             <ul style={{ paddingLeft: "0" }}>
-              {data &&
-                data.map((datum) => {
-                  return (
-                    datum.location === "zone_2" && (
-                      <DisplayItem datum={datum} key={datum.device} />
-                    )
-                  );
-                })}
+              {zone3Data.map((datum) => {
+                return <DisplayItem datum={datum} key={datum.device} />;
+              })}
             </ul>
           </div>
 
           <div className="inner-square">
             <ul style={{ paddingLeft: "0" }}>
-              {data &&
-                data.map((datum) => {
-                  return (
-                    datum.location === "zone_3" && (
-                      <DisplayItem datum={datum} key={datum.device} />
-                    )
-                  );
-                })}
-            </ul>
-          </div>
-
-          <div className="inner-square">
-            <ul style={{ paddingLeft: "0" }}>
-              {data &&
-                data.map((datum) => {
-                  return (
-                    datum.location === "zone_4" && (
-                      <DisplayItem datum={datum} key={datum.device} />
-                    )
-                  );
-                })}
+              {zone4Data.map((datum) => {
+                return <DisplayItem datum={datum} key={datum.device} />;
+              })}
             </ul>
           </div>
         </div>
