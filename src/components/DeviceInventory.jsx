3cf35f5803b4e53e5388f51624ff75162ed5e32d
William Rose <willrose999@gmail.com>
2023-01-01 20:29:35 -0500

    Refresh button and updated inventory display

--- src/components/DeviceInventory.jsx
+++ src/components/DeviceInventory.jsx
@@ -2,11 +2,23 @@ function DeviceInventory({ datum }) {
   const { device, location, timestamp } = datum;
   return (
     <>
-      <ul className="device-inventory-instance">
-        <h3>Device ID: {device}</h3>
-        <li>Location: {location}</li>
-        <li>Time Stamp: {timestamp}</li>
-      </ul>
+      {device === "6ATF12018" && (
+        <ul className="device-inventory-instance">
+          <h3>Device ID: {device}</h3>
+          <h3>Kuzi</h3>
+          <li>Location: {location}</li>
+          <li>Time Stamp: {timestamp}</li>
+        </ul>
+      )}
+
+      {device === "6ATF12016" && (
+        <ul className="device-inventory-instance">
+          <h3>Device ID: {device}</h3>
+          <h3>William</h3>
+          <li>Location: {location}</li>
+          <li>Time Stamp: {timestamp}</li>
+        </ul>
+      )}
     </>
   );
 }
--- src/components/DisplayTwo.jsx
+++ src/components/DisplayTwo.jsx
@@ -14,22 +14,35 @@ const getData = async () => {
 function DisplayTwo() {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isRefresh, setIsRefresh] = useState(false);
+
+  const refreshButtonHandler = () => {
+    if (isRefresh) {
+      setIsRefresh(false);
+      console.log("Set to False");
+    } else {
+      setIsRefresh(true);
+      console.log("Set to True");
+    }
+  };
 
   useEffect(() => {
     getData().then((response) => {
       setData(response);
       setIsLoading(false);
     });
-  }, []);
+  }, [isRefresh]);
 
   const zone1Data = [];
   const zone2Data = [];
   const zone3Data = [];
   const zone4Data = [];
+  const zone5Data = [];
+  const zone6Data = [];
 
   if (data) {
     data.forEach((datum) => {
-      if (datum.location === "zone_1") {
+      if (datum.location === "zone_1" && datum.device.includes("ATF")) {
         zone1Data.push(datum);
       } else if (datum.location === "zone_2") {
         zone2Data.push(datum);
@@ -37,6 +50,10 @@ function DisplayTwo() {
         zone3Data.push(datum);
       } else if (datum.location === "zone_4") {
         zone4Data.push(datum);
+      } else if (datum.location === "zone_5") {
+        zone5Data.push(datum);
+      } else if (datum.location === "zone_6") {
+        zone6Data.push(datum);
       }
     });
   }
@@ -52,36 +69,62 @@ function DisplayTwo() {
               return <DeviceInventory datum={datum} key={datum.device} />;
             })}
         </div>
-        <div className="container">
-          <div className="inner-square">
-            <ul style={{ paddingLeft: "0" }}>
-              {zone1Data.map((datum) => {
-                return <DisplayItem datum={datum} key={datum.device} />;
-              })}
-            </ul>
+        <div className="zone-grid">
+          <div style={{ textAlign: "center" }}>
+            <h3>Warehouse: DSC3</h3>
           </div>
-          <div className="inner-square">
-            <ul style={{ paddingLeft: "0" }}>
-              {zone2Data.map((datum) => {
-                return <DisplayItem datum={datum} key={datum.device} />;
-              })}
-            </ul>
+          <div className="container">
+            <div className="inner-square">
+              <ul style={{ paddingLeft: "0" }}>
+                <h6>Zone 4</h6>
+                {zone4Data.map((datum) => {
+                  return <DisplayItem datum={datum} key={datum.device} />;
+                })}
+              </ul>
+            </div>
+            <div className="inner-square">
+              <ul style={{ paddingLeft: "0" }}>
+                <h6>Zone 5</h6>
+                {zone5Data.map((datum) => {
+                  return <DisplayItem datum={datum} key={datum.device} />;
+                })}
+              </ul>
+            </div>
+            <div className="inner-square">
+              <ul style={{ paddingLeft: "0" }}>
+                <h6>Zone 6</h6>
+                {zone6Data.map((datum) => {
+                  return <DisplayItem datum={datum} key={datum.device} />;
+                })}
+              </ul>
+            </div>
+            <div className="inner-square">
+              <ul style={{ paddingLeft: "0" }}>
+                <h6>Zone 3</h6>
+                {zone3Data.map((datum) => {
+                  return <DisplayItem datum={datum} key={datum.device} />;
+                })}
+              </ul>
+            </div>
+            <div className="inner-square">
+              <ul style={{ paddingLeft: "0" }}>
+                <h6>Zone 2</h6>
+                {zone2Data.map((datum) => {
+                  return <DisplayItem datum={datum} key={datum.device} />;
+                })}
+              </ul>
+            </div>
+            <div className="inner-square">
+              <ul style={{ paddingLeft: "0" }}>
+                <h6>Zone 1</h6>
+                {zone1Data.map((datum) => {
+                  return <DisplayItem datum={datum} key={datum.device} />;
+                })}
+              </ul>
+            </div>
           </div>
-
-          <div className="inner-square">
-            <ul style={{ paddingLeft: "0" }}>
-              {zone3Data.map((datum) => {
-                return <DisplayItem datum={datum} key={datum.device} />;
-              })}
-            </ul>
-          </div>
-
-          <div className="inner-square">
-            <ul style={{ paddingLeft: "0" }}>
-              {zone4Data.map((datum) => {
-                return <DisplayItem datum={datum} key={datum.device} />;
-              })}
-            </ul>
+          <div className="refresh-button-div">
+            <button onClick={refreshButtonHandler}>Update</button>
           </div>
         </div>
       </div>
