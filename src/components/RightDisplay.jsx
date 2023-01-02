21052e0a2f83d908c62fd8a1f7648563a0489c1f
William Rose <willrose999@gmail.com>
2023-01-02 01:15:25 -0500

    Updated UI

---
+++ src/components/RightDisplay.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/system";
+import { useContext } from "react";
+import LocationContext from "../context/LocationContext";
+import DeviceInventory from "./shared/DeviceInventory";
+function RightDisplay() {
+  const { data } = useContext(LocationContext);
+
+  return (
+    <Box flex={1}>
+      <div className="device-inventory-container">
+        {data &&
+          data.map((datum) => {
+            return <DeviceInventory datum={datum} key={datum.device} />;
+          })}
+      </div>
+    </Box>
+  );
+}
+
+export default RightDisplay;
